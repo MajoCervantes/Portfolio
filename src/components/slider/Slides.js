f3527bb720158a100a0681dbc2ca0b84243c1da4
MajoCervantes <champiis49@gmail.com>
2022-01-13 20:08:10 -0600

    Added links to proyects

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -41,7 +41,7 @@ const slidesInfo = [
 
 const slides = slidesInfo.map((slide, idx) => (
     <div key={idx} className="slide-container">
-        <a href={`${slide.src === Trivia ? "https://trivia-two.vercel.app/</div>" : slide.src === ToDo ? "https://to-do-list-sand.vercel.app/" : slide.src === Github ? "https://github-user-three.vercel.app/" : slide.src === eCommerce ? "https://e-commerce-rho-sage.vercel.app/" : "https://weather-app-virid-three.vercel.app/"}`}>
+        <a href={`${slide.src === Trivia ? "https://trivia-two.vercel.app/</div>" : slide.src === ToDo ? "https://to-do-list-sand.vercel.app/" : slide.src === Github ? "https://github-user-three.vercel.app/" : slide.src === eCommerce ? "https://e-commerce-rho-sage.vercel.app/" : "https://weather-app-virid-three.vercel.app/"}`} target="_blank">
             <img src={slide.src} alt={slide.alt} />
         </a>
         <div className="slide-desc">
